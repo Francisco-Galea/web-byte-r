7b1c3ab350916b5ecf2283915c8c3fd3da50e2c3
Francisco <galeafrancisco2001@gmail.com>
2025-11-03 14:01:11 -0300

    Added all app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,16 @@ import { Main } from './pages/main/main';
 import { Projects } from './pages/projects/projects';
 import { Contact } from './pages/contact/contact';
 import { AboutUs } from './pages/about-us/about-us';
+import { Services } from './pages/services/services';
 
 export const routes: Routes = [
     {
         path: '',
         component: Main
+    },
+    {
+        path: 'services',
+        component: Services
     },
     {
         path: 'projects',
